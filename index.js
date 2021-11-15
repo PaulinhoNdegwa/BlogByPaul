8df0ee8a7afb9cc4b6f797481cbcbec824c343aa
Paul Gichuki <pherndegz@gmail.com>
2021-11-15 11:30:42 +0300

    Implement user registration and login

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+// Routers
+const authRouter = require('./routes/auth');
+
+dotenv.config()
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("Connected to MongoDB"))
 .catch((error) => console.log(error))
 
+app.use("/api/auth", authRouter);
 
 app.listen("5000", () => {
     console.log("Server up and running...")
---
+++ routes/auth.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+// Register
+router.post('/register', async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPass = await bcrypt.hash(req.body.password, salt)
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPass,
+        });
+        const user = await newUser.save();
+        res.status(200).json({
+            "message": "user successfully created",
+            "user": user
+        });
+    } catch (error) {
+        res.status(500).json({
+            "message": "error when creating user account",
+            "error": error.message
+        });
+    }
+})
+
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if(!user){
+            console.log("User not found")
+            res.status(400).json({
+                "message": "wrong credentials"
+            });
+            return;
+        }
+
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        if(!validated){
+            console.log("Wrong password")
+            res.status(400).json({
+                "message": "wrong credentials"
+            });
+            return;
+        }
+        const { password, ...others } = user._doc;
+
+        res.status(200).json({
+            "message": "login successful",
+            "user": others
+        })
+    } catch (error) {
+        res.status(500).json({
+            "message": "error when logging in",
+            "error": error.message
+        });
+    }
+})
+
+module.exports = router;
